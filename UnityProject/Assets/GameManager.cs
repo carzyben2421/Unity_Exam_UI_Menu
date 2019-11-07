877a8532afda9c8f4806d97e023b4949d31ea056
carzyben2421 <55227126+carzyben2421@users.noreply.github.com>
2019-11-07 14:42:17 +0800

    1107

---
+++ UnityProject/Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public Text loadingText;
+    public Slider loading;
+
+    public void SetVBGM(float value)
+    {
+        mixer.SetFloat("VBGM", value);
+    }
+    public void SetVSFX(float value)
+    {
+        mixer.SetFloat("VSFX", value);
+    }
+}
